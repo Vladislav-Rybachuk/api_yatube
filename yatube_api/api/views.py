e75809ee35e832d73abbaf4ceddfcf14103ee9b1
Владислав Рыбачук <vlad1k-o@yandex.ru>
2023-02-03 23:58:57 +0300

    E128

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -4,8 +4,8 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 
 from api.permissions import IsAuthorOrReadOnly
-from api.serializers import (CommentSerializer, GroupSerializer,
-    PostSerializer, UserSerializer)
+from api.serializers import (
+    CommentSerializer, GroupSerializer,PostSerializer, UserSerializer)
 from posts.models import Group, Post, User
 
 
